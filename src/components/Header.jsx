ad3a09e9cbb20100fa2b612159cd128e5a21cf73
Jisun696 <istiakkashem35@gmail.com>
2024-04-07 00:57:46 +0600

    installed moment.js and completed header part

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import logo from '../assets/images/logo.png';
+import moment from 'moment';
 
 const Header = () => {
   return (
-    <header className="flex justify-center">
+    <header className="flex flex-col items-center justify-center font-poppins pt-6">
       <img src={logo} alt="site logo" />
+      <p className="text-neutral-500 text-lg font-normal mt-5 mb-2">Journalism Without Fear or Favour</p>
+      <p className="text-neutral-500 text-xl font-medium">
+        <span className="text-neutral-700">{moment().format('dddd,')}</span> {moment().format('MMMM D, YYYY')}
+      </p>
     </header>
   );
 };
