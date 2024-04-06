57dab78b76db8daa2fec0c04da024f97a931ded3
Jisun696 <istiakkashem35@gmail.com>
2024-04-07 00:27:30 +0600

    installed react-router-dom and seted up

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/images/logo.png';
+
+const Header = () => {
+  return (
+    <header className="flex justify-center">
+      <img src={logo} alt="site logo" />
+    </header>
+  );
+};
+
+export default Header;
