e87f2f435fd7f36bbc2ba6620c9bbc46c91ebce2
Jisun696 <istiakkashem35@gmail.com>
2024-04-07 01:32:39 +0600

    installed react-fast-marquee and used in the newly added MarqueeSlider component

---
+++ src/components/MarqueeSlider.jsx
@@ -0,0 +1,14 @@
+import Marquee from 'react-fast-marquee';
+
+const MarqueeSlider = () => {
+  return (
+    <div className="p-4 bg-zinc-100 flex gap-2 items-center mt-7">
+      <span className="py-3 px-6 bg-rose-600 text-white text-xl font-medium leading-none">Latest</span>
+      <Marquee autoFill={true} pauseOnHover={true} speed={100} className="text-neutral-700 text-lg font-semibold ">
+        Match Highlights : Germany vs Spain — as it happened ! &emsp;
+      </Marquee>
+    </div>
+  );
+};
+
+export default MarqueeSlider;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import Header from '@/components/Header';
+import MarqueeSlider from '@/components/MarqueeSlider';
 import React from 'react';
 
 const Home = () => {
   return (
     <>
       <Header />
+      <main className="container mx-auto font-poppins">
+        <MarqueeSlider />
+      </main>
     </>
   );
 };
