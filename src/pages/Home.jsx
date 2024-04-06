81aad5bad68cdbe846824da3f082ad4b67edef05
Jisun696 <istiakkashem35@gmail.com>
2024-04-07 05:14:57 +0600

    changed some styles and positions and also added the layout structure for the main section of homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,15 @@ import React from 'react';
 const Home = () => {
   return (
     <>
-      <Header />
-      <main className="container mx-auto font-poppins">
+      <header className="container mx-auto font-poppins">
+        <Header />
         <MarqueeSlider />
         <Nav />
+      </header>
+      <main className="container mx-auto font-poppins grid grid-cols-4 gap-5">
+        <div className="border border-red-400">Left side nav</div>
+        <div className="border border-green-400 col-span-2">main news</div>
+        <div className="border border-blue-400">Right side nav</div>
       </main>
     </>
   );
